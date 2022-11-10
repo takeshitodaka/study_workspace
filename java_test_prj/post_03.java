5e7f33a0c3ebbc5f0171b278ccde28d590cdefaf
Takeshi Todaka <bichoamante@yahoo.co.jp>
2022-11-11 04:59:04 +0900

    ""

--- java_test_prj/post_03.java
+++ java_test_prj/post_03.java
@@ -4,20 +4,11 @@ class post02 {
     public static void main(String args[])throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int i, val;
+       int a=1, b=2;
+       double c;
+       c=a/b;
 
-        System.out.print("長さ：");
-        val = Integer.parseInt(br.readLine());
-
-        //ここからプログラムを記述してください。
-        for(i=0;i<val;i++){
-            if(i%4<2){
-                System.out.print("+");
-            }else{
-
-                System.out.print("-");
-            }
-        }
-        System.out.println("");
+        System.out.println(c);
     }
 }
+l=1
