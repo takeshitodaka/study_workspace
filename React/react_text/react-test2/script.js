78d96ff66fb45192f19ac612648034ab2124a5ac
Takeshi Todaka <bichoamante@yahoo.co.jp>
2022-11-15 07:51:41 +0900

    ""

---
+++ React/react_text/react-test2/script.js
@@ -0,0 +1,32 @@
+let counter = 0
+let dom  = document.querySelector('#root')
+doCount()
+function doCount(){
+    counter++;
+    let element = React.createElement(
+        'div', {}, [
+            React.createElement(
+                'h2',{},"Hello!"
+            ),
+            React.createElement(
+                'h4',{className: 'alert alert-primary'},"React sample page."
+            ),
+            React.createElement(
+                'ul',{className: 'list-group'},[
+            
+                    React.createElement(
+                        'li',{className: 'list-group-item'},"First item"
+                    ),
+                    React.createElement(
+                        'li',{className: 'list-group-item'},"Second item"
+                    ),
+                    React.createElement(
+                        'li',{className: 'list-group-item'},"Third item"
+                    ),
+                ]
+            ),
+        ]
+    )
+    ReactDOM.render(element,dom)
+    
+}
